871515d61c400608f2ce0f89b7d887f925170563
Fernando Campos da Cruz <fernandocdacruz@gmail.com>
2024-04-22 13:10:56 -0300

    2 commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,44 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import utilities.Menu;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("@@@                               @@@");
+		System.out.println("@@@   Jeba Bank of Brazil - JBB   @@@");
+		System.out.println("@@@                               @@@");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println();
+		System.out.println("Bem vindo ao sistema de controle de funcionários do JBB !!");
+		
+		Menu menu = new Menu(sc);
+		
+		menu.showMenu();
+		
+		int op = menu.getOp();
+		
+		while (op != 0) {
+			switch (op) {
+			case 1:
+				System.out.println("teste1");
+				menu.showMenu();
+				op = menu.getOp();
+			}
+		}
+		
+		sc.close();
 	}
 
 }
