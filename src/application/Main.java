7231334bddcf3819027db389d70831944abb7b15
Fernando Campos da Cruz <fernandocdacruz@gmail.com>
2024-04-23 12:05:12 -0300

    4 commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Departament;
+import entities.Worker;
 import utilities.Menu;
+import utilities.enums.WorkerLevel;
 
 public class Main {
 
@@ -29,16 +34,66 @@ public class Main {
 		
 		int op = menu.getOp();
 		
+		List<Worker> workers = new ArrayList<>();
+		
 		while (op != 0) {
 			switch (op) {
 			case 1:
-				System.out.println("teste1");
+				System.out.print("Quantos funcionários vão ser cadastrados? ");
+				int numFuncionarios = sc.nextInt();
+				System.out.println();
+				for (int i = 1; i <= numFuncionarios; i++) {
+					System.out.println("Funcionário #" + i);
+					System.out.print("Id: ");
+					sc.nextLine();
+					int id = sc.nextInt();
+					while (hasId(workers, id)) {
+						System.out.println();
+						System.out.println("Id já cadastrado. Tente outro.");
+						System.out.print("Id: ");
+						id = sc.nextInt();
+					}
+					System.out.print("Departamento: ");
+					sc.nextLine();
+					String dep = sc.nextLine();
+					System.out.print("Nome: ");
+					String nome = sc.nextLine();
+					System.out.print("Nível: ");
+					System.out.println();
+					System.out.println("[1] - Junior\n[2] - Pleno\n[3] - Senior");
+					int nivelOp = sc.nextInt();
+					while (nivelOp < 1 || nivelOp > 3) {
+						System.out.println("Opção inválida. Tente Novamente.");
+						System.out.println();
+						System.out.println("[1] - Junior\n[2] - Pleno\n[3] - Senior");
+						nivelOp = sc.nextInt();
+					}
+					String nivel = "";
+					switch (nivelOp) {
+						case 1: nivel = "JUNIOR";
+						break;
+						case 2 : nivel = "MID_LEVEL";
+						break;
+						case 3 : nivel = "SENIOR";
+						break;
+					}
+					System.out.print("Salário: ");
+					double salario = sc.nextDouble();
+					workers.add(new Worker(id, nome, WorkerLevel.valueOf(nivel), salario, new Departament(dep)));
+					System.out.println();
+				}
+				System.out.println("Cadastro feito com sucesso.");
 				menu.showMenu();
 				op = menu.getOp();
 			}
 		}
 		
 		sc.close();
+	}
+	
+	public static boolean hasId(List<Worker> list , int id) {
+		Worker worker = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return worker != null;
 	}
 
 }
--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import utilities.enums.WorkerLevel;
@@ -25,6 +26,74 @@ public class Worker {
 		this.baseSalary = baseSalary;
 		this.departament = departament;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void  addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for(HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+	
+	
 	
 	
 	
